8f69261f6000643748c16a78646c22178f427129
LinuCN <linucn777@gmail.com>
2017-06-19 12:42:38 +0530

    Project Initializing

---
+++ app/src/main/java/org/openweather/apis/ApiClient.java
@@ -0,0 +1,17 @@
+package org.openweather.apis;
+
+
+import com.google.gson.Gson;
+
+import org.openweather.Model.WeatherInfoModel;
+
+public class ApiClient {
+
+	private static final string API_WEATHER = "";
+
+	final Gson gson = new Gson();
+	WeatherInfoModel getWeatherDetails(){
+
+	}
+
+}
---
+++ app/src/main/java/org/openweathermap/openweathermap/SelectedLocationListActivity.java
@@ -0,0 +1,13 @@
+package org.openweathermap.openweathermap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectedLocationListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_location_list);
+    }
+}
